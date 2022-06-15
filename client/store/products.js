37348d9792280583d9d58fdb5e2de2c2be93d951
Nathan Needham <needham.nathan@gmail.com>
2022-06-15 07:48:24 -0700

    refactored intialstate into products.js

--- client/store/products.js
+++ client/store/products.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import {GOT_ALL_PRODUCTS} from './actionTypes.js'
-import {initialState} from './intialState.js'
+
+const initialState = {
+  users: [],
+  products: []
+}
 
 //ACTION CREATOR
 export const setProducts = (products) => {
