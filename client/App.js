b9a3da8546a0937c54c58d402905e9bd0a4b654f
kwangric <43795498+kwangric@users.noreply.github.com>
2022-06-15 17:37:46 -0400

    Cleaned up App.js

--- client/App.js
+++ client/App.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 import Navbar from './components/Navbar'
 import Routes from './Routes'
-import SingleUser from './components/SingleUser'
-import AllUsers from './components/AllUsers'
-import Cart from './components/Cart'
-import CartLink from './components/CartLink'
 
 const App = () => {
   return (
