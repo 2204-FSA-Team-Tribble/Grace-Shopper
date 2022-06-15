677f4ce58c465735c8e5f3409df048eb8de2deca
kwangric <43795498+kwangric@users.noreply.github.com>
2022-06-15 11:56:25 -0400

    Converted AllUsers to class component

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,87 +1,91 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import { setUsers } from '../store/users.js'
 
-const AllUsers = () => {
-  const [users, setUsers] = useState([])
+// Placeholder user array
+const testUsers = [
+  {
+    id: 1,
+    firstName: 'Bob',
+    lastName: 'Smith',
+    address: '100 Pine Rd.',
+    username: 'bobthedog',
+    email: 'bobthedog@gmail.com',
+    password: 'ilovedogs1',
+  },
+  {
+    id: 2,
+    firstName: 'Lisa',
+    lastName: 'Frank',
+    address: '311 Oak St.',
+    username: 'lisafrank100',
+    email: 'lisafrank100@gmail.com',
+    password: 'ihatedogs1',
+  },
+  {
+    id: 3,
+    firstName: 'Sarah',
+    lastName: 'Frank',
+    address: '311 Oak St.',
+    username: 'sarahfrank100',
+    email: 'sarahfrank100@gmail.com',
+    password: 'ilovecats1',
+  },
+]
 
-  // Placeholder user array
-  const testUsers = [
-    {
-      id: 1,
-      firstName: 'Bob',
-      lastName: 'Smith',
-      address: '100 Pine Rd.',
-      username: 'bobthedog',
-      email: 'bobthedog@gmail.com',
-      password: 'ilovedogs1',
-    },
-    {
-      id: 2,
-      firstName: 'Lisa',
-      lastName: 'Frank',
-      address: '311 Oak St.',
-      username: 'lisafrank100',
-      email: 'lisafrank100@gmail.com',
-      password: 'ihatedogs1',
-    },
-    {
-      id: 3,
-      firstName: 'Sarah',
-      lastName: 'Frank',
-      address: '311 Oak St.',
-      username: 'sarahfrank100',
-      email: 'sarahfrank100@gmail.com',
-      password: 'ilovecats1',
-    },
-  ]
+export class AllUsers extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-  // Assign users to state, dispatch will go here
-  useEffect(() => {
-    setUsers(testUsers)
-  }, [])
+  componentDidMount() {
+    this.props.setUsers()
+  }
 
-  return (
-    <div>
-      <h3>All Users</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr className="user" key={user.id}>
-                <td>{`${user.firstName} ${user.lastName}`}</td>
-                <td>{user.address}</td>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
-                <td>
-                  {user.password
-                    .split('')
-                    .map((char) => '*')
-                    .join('')}
-                </td>
-                <td>
-                  <a href="#">Edit</a>
-                </td>
-                <td>
-                  <a href="#">Remove</a>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
-  )
+
+  render() {
+    return (
+      <div>
+        <h3>All Users</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.users.map((user) => {
+              return (
+                <tr className="user" key={user.id}>
+                  {/* <td>{`${user.firstName} ${user.lastName}`}</td>
+                  <td>{user.address}</td> */}
+                  <td>{user.username}</td>
+                  {/* <td>{user.email}</td>
+                  <td>
+                    {user.password
+                      .split('')
+                      .map((char) => '*')
+                      .join('')}
+                  </td> */}
+                  <td>
+                    <a href="#">Edit</a>
+                  </td>
+                  <td>
+                    <a href="#">Remove</a>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => ({
@@ -89,7 +93,7 @@ const mapState = (state) => ({
 })
 
 const mapDispatch = (dispatch) => ({
-  getUsers: () => dispatch(getUsers())
+  setUsers: () => dispatch(setUsers()),
 })
 
-export default connect(mapState, mapDispatch)(AllUsers);
+export default connect(mapState, mapDispatch)(AllUsers)
--- client/store/index.js
+++ client/store/index.js
@@ -4,17 +4,16 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 import users from './users'
-import user from './singleUser'
 import cart from './cart'
 import productsReducer from './products'
 
 const reducer = combineReducers({
   auth: auth,
   users: users,
-  user: user,
   cart: cart,
   products: productsReducer,
  })
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
