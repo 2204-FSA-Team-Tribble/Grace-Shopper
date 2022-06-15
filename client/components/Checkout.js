ceafad62d9dec9f6aed361beedd4dc76148a9960
alli-g <acgokbudak@gmail.com>
2022-06-15 16:00:15 -0600

    Checkout page started

---
+++ client/components/Checkout.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Form from 'react-bootstrap/Form';
+
+let cartPlaceHolder = [
+  {
+    name: 'GAPZER Anti-Scratch Cat Shoes',
+    image: 'https://m.media-amazon.com/images/I/71EbAjac+pL._AC_UL320_.jpg',
+    petType: 'cat',
+    clothingType: 'shoes',
+    price: 12.99,
+  },
+  {
+    name: 'Dog Life Jackets, Ripstop Pet Floatation ',
+    image: 'https://m.media-amazon.com/images/I/71vgo2wI0NL._AC_UL320_.jpg',
+    petType: 'dog',
+    clothingType: 'swim',
+    price: 43.99,
+  },
+  {
+    name: 'CooShou 4 Pieces Bunny Clothes',
+    image: 'https://m.media-amazon.com/images/I/71ISX-nW6EL._AC_UL320_.jpg',
+    petType: 'other',
+    clothingType: 'shirt',
+    price: 23.99,
+  },
+];
+//Make sure to pass the isLoggedIn into the props.
+
+export default class Checkout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: cartPlaceHolder,
+      user: {},
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Checkout Page</h3>
+      </div>
+    /*
+         FEATURES FOR THIS PAGE
+         Show the list of XitemsX, total.
+         Field to enter Address (populated with address if logged in)
+         Field to enter payment
+         Submit button that gives a "You did it" pop up
+        */
+
+    /* {cart.map((item, index) => {
+            return (
+              <div className="cart-item" key={index}>
+                <img src={item.image} />
+                <div>
+                  <p>{item.name}</p>
+                  <strong>${item.price}</strong>
+                  <p>{item.description}</p>
+                  <span>Qty: </span>
+                </div>
+              </div>
+            );
+          })}
+          <div>TOTAL:</div>
+          {}
+          <button type="button">Complete checkout</button>
+         */
+    );
+  }
+}
+
+const mapState = (state) => ({
+  cart: state.cart,
+  user: state.user,
+  auth: state.auth,
+});
+
+const mapDispatch = (dispatch) => ({
+  getCart: (id) => dispatch(getCart(id)),
+});
+
+export default connect(mapState, mapDispatch)(Checkout);
+
--- client/store/index.js
+++ client/store/index.js
@@ -5,21 +5,13 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 import users from './users'
 import cart from './cart'
-import user from './singleUser'
 import productsReducer from './products'
-import deleteProductReducer from './deleteProduct'
-import createProductReducer from './createProduct'
-import singleProductReducer from './singleProduct'
 
 const reducer = combineReducers({
   auth: auth,
   users: users,
-  user: user,
   cart: cart,
   products: productsReducer,
-  deleteProduct: deleteProductReducer,
-  createProduct: createProductReducer,
-  singleProduct: singleProductReducer
  })
 
 const middleware = composeWithDevTools(
