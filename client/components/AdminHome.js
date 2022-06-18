0bb372f756a6355b12862a257ea0e216394f975b
Nathan Needham <65629528+nneedham@users.noreply.github.com>
2022-06-18 11:12:41 -0700

    fixed react class vs classname error message

--- client/components/AdminHome.js
+++ client/components/AdminHome.js
@@ -14,39 +14,39 @@ export class AdminHome extends React.Component {
       <div>
         {user.isAdmin ? (
           <div>
-            <div class="container">
-              <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-              <h1 class="display-4">Hello Administrator</h1>
-              <p class="lead">Here you can quickly update both users and products. Click on the following links to start making your adjustments.</p>
+            <div className="container">
+              <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+              <h1 className="display-4">Hello Administrator</h1>
+              <p className="lead">Here you can quickly update both users and products. Click on the following links to start making your adjustments.</p>
               </div>
             </div>
-            <div class="container">
-              <div class="card-deck mb-3 text-center">
-                <div class="card mb-4 shadow-sm">
-                  <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Products</h4>
+            <div className="container">
+              <div className="card-deck mb-3 text-center">
+                <div className="card mb-4 shadow-sm">
+                  <div className="card-header">
+                    <h4 className="my-0 font-weight-normal">Products</h4>
                   </div>
-                  <div class="card-body">
-                    <p class="lead">Here you can see all products, update products, create new products and delete products.</p>
+                  <div className="card-body">
+                    <p className="lead">Here you can see all products, update products, create new products and delete products.</p>
                   </div>
-                  <div class="container">
+                  <div className="container">
                     <Link to="/productsadmin">
-                      <button type="button" class="btn btn-lg btn-block btn-outline-secondary">Manage Products</button>
+                      <button type="button" className="btn btn-lg btn-block btn-outline-secondary">Manage Products</button>
                     </Link>
                   </div>
                 </div>
               </div>
-              <div class="card-deck mb-3 text-center">
-                <div class="card mb-4 shadow-sm">
-                  <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Users</h4>
+              <div className="card-deck mb-3 text-center">
+                <div className="card mb-4 shadow-sm">
+                  <div className="card-header">
+                    <h4 className="my-0 font-weight-normal">Users</h4>
                   </div>
-                  <div class="card-body">
-                    <p class="lead">Here you can see all users, update users and create new users.</p>
+                  <div className="card-body">
+                    <p className="lead">Here you can see all users, update users and create new users.</p>
                   </div>
-                  <div class="container">
+                  <div className="container">
                     <Link to="/users">
-                      <button type="button" class="btn btn-lg btn-block btn-outline-secondary">Manage Users</button>
+                      <button type="button" className="btn btn-lg btn-block btn-outline-secondary">Manage Users</button>
                     </Link>
                   </div>
                 </div>
