2fc802242ea030c3deae216489ab4bc2ba13ad3f
Nike Ojetola <ojetolanike@gmail.com>
2022-06-23 04:25:59 -0400

    added name to singleProduct

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fetchProduct } from '../store/singleProduct.js';
-import { addProduct } from '../store/cart.js'
+import { addProduct } from '../store/cart.js';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
@@ -19,7 +19,7 @@ class SingleProduct extends React.Component {
 
   render() {
     let product = this.props.product.product || {};
-    const userId = this.props.auth.id
+    const userId = this.props.auth.id;
 
     const Loading = () => {
       return <>Loading....</>;
@@ -38,18 +38,26 @@ class SingleProduct extends React.Component {
           <div className="col-md-6">
             <h4 className="text-uppercase text-black-50">{product.petType}</h4>
             <h3 className="display-7">{product.clothingType}</h3>
+            <h3 className="display-7">{product.name}</h3>
             <h3 className="display-6 fw-bold my-4">${product.price}</h3>
             <p className="lead">{product.description}</p>
             {userId > -1 ? (
-            <><button
-              className="btn btn-outline-secondary px-4 py-2"
-              onClick={() => this.props.addProduct(this.props.auth.id, product)}
-            >
-              Add to Cart
-            </button>
-            <NavLink to="/cart" className="btn btn-secondary ms-2 px-3">
-              Go to Cart
-            </NavLink></>) : <></>}
+              <>
+                <button
+                  className="btn btn-outline-secondary px-4 py-2"
+                  onClick={() =>
+                    this.props.addProduct(this.props.auth.id, product)
+                  }
+                >
+                  Add to Cart
+                </button>
+                <NavLink to="/cart" className="btn btn-secondary ms-2 px-3">
+                  Go to Cart
+                </NavLink>
+              </>
+            ) : (
+              <></>
+            )}
           </div>
         </>
       );
@@ -68,13 +76,13 @@ class SingleProduct extends React.Component {
 }
 
 const mapState = (state) => {
-  return { product: state.singleProduct, auth: state.auth, };
+  return { product: state.singleProduct, auth: state.auth };
 };
 
 const mapDispatch = (dispatch) => {
   return {
     fetchProduct: (id) => dispatch(fetchProduct(id)),
-    addProduct: (userId, product) => dispatch(addProduct(userId, product))
+    addProduct: (userId, product) => dispatch(addProduct(userId, product)),
   };
 };
 
